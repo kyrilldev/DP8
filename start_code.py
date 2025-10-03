131955312393f7c2fe536d02c16c4110b4ff02b4
Kyrill15 <52017853+kyrilldev@users.noreply.github.com>
2025-10-03 11:21:31 +0200

    minimum reqs done

--- start_code.py
+++ start_code.py
@@ -3,61 +3,95 @@ from pathlib import Path
 import json
 import pprint
 from database_wrapper import Database
+import random
+from enum import Enum
 
+
+class bevoegdheid(Enum):
+    '''Helps simplify selecting tasks'''
+    STAGIAIRE = "Stagiaire",
+    JUNIOR = "Junior",
+    MEDIOR = "Medior",
+    SENIOR = "Senior"
 
 # initialisatie
 
+RAIN_CHANCE = random.randint(0,100)
+
 # parameters voor connectie met de database
-db = Database(host="localhost", gebruiker="root", wachtwoord="Lily8-Pancake7", database="sportcompetitie")
+db = Database(host="localhost", gebruiker="root", wachtwoord="Lily8-Pancake7", database="attractiepark")
 
 
 # main
+
 
 # Haal de eigenschappen op van een personeelslid
 # altijd verbinding openen om query's uit te voeren
 db.connect()
 
 # pas deze query aan om het juiste personeelslid te selecteren
-select_query = "SELECT * FROM sportcompetitie WHERE speeldag = 'Zaterdag'"
-personeelslid = db.execute_query(select_query)
+select_query = "SELECT * FROM personeelslid"
+personeelsleden = db.execute_query(select_query)
 
 # altijd verbinding sluiten met de database als je klaar bent
 db.close()
 
-pprint.pp(personeelslid) # print de resultaten van de query op een overzichtelijke manier
-# print(personeelslid[0]['naam']) # voorbeeld van hoe je bij een eigenschap komt
+def bereken_maximale_belasting(personeelslid) -> int:
+    '''berekend de maximale belasting van een personeelslid'''
+    leeftijd: int = personeelslid['leeftijd']
+    verlaagde_fysieke_belasting: int  = personeelslid['verlaagde_fysieke_belasting']
 
-
-
-# Haal alle onderhoudstaken op
-# altijd verbinding openen om query's uit te voeren
+    if verlaagde_fysieke_belasting == 0:
+        if leeftijd <= 24:
+            return 25
+        if leeftijd >= 25 and leeftijd <= 50:
+            return 40
+        if leeftijd > 51:
+            return 20
+    else:
+        return verlaagde_fysieke_belasting
+    
 db.connect()
 
-# pas deze query aan en voeg queries toe om de juiste onderhoudstaken op te halen
-select_query = "SELECT * FROM sportcompetitie"
-onderhoudstaken = db.execute_query(select_query)
-
-# altijd verbinding sluiten met de database als je klaar bent
+query = "SELECT * FROM onderhoudstaak"
+onderhoudstaken = db.execute_query(query)
+# pprint.pp(onderhoudstaken)
 db.close()
 
-#pprint.pp(onderhoudstaken) # print de resultaten van de query op een overzichtelijke manier
+user_taken = []
+
+pprint.pp(personeelsleden[0])
+
+# verzamel taken
+for taak in onderhoudstaken:
+    beroepstype = taak['beroepstype']
+    bevoegdheid = taak['bevoegdheid']
+    fysieke_belasting = taak['fysieke_belasting']
+
+    if beroepstype == personeelsleden[0]['beroepstype'] and bevoegdheid <= personeelsleden[0]['bevoegdheid'] and bereken_maximale_belasting(personeelslid=personeelsleden[0]) >= fysieke_belasting:
+        user_taken.append(taak)
+        print(beroepstype)
+        print(bevoegdheid)
+        print(fysieke_belasting)
+
+# bereken taak duur
+totale_duur = 0
+for taak in user_taken:
+    totale_duur += taak['duur']
 
 
 
-# # verzamel alle benodigde gegevens in een dictionary
-# dagtakenlijst = {
-#     "personeelsgegevens" : {
-#         "naam": personeelslid[0]['naam'] # voorbeeld van hoe je bij een eigenschap komt
-#         # STAP 1: vul aan met andere benodigde eigenschappen
-#     },
-#     "weergegevens" : {
-#         # STAP 4: vul aan met weergegevens
-#     }, 
-#     "dagtaken": [] # STAP 2: hier komt een lijst met alle dagtaken
-#     ,
-#     "totale_duur": 0 # STAP 3: aanpassen naar daadwerkelijke totale duur
-# }
+# verzamel alle benodigde gegevens in een dictionary
+dagtakenlijst = {
+    "personeelsgegevens" : {
+        "naam": personeelsleden[0] # voorbeeld van hoe je bij een eigenschap komt
+        # STAP 1: vul aan met andere benodigde eigenschappen
+    },
+    "dagtaken": user_taken # STAP 2: hier komt een lijst met alle dagtaken
+    ,
+    "totale_duur": totale_duur # STAP 3: aanpassen naar daadwerkelijke totale duur
+}
 
-# # uiteindelijk schrijven we de dictionary weg naar een JSON-bestand, die kan worden ingelezen door de acceptatieomgeving
-# with open('dagtakenlijst_personeelslid_x.json', 'w') as json_bestand_uitvoer:
-#     json.dump(dagtakenlijst, json_bestand_uitvoer, indent=4)
+# uiteindelijk schrijven we de dictionary weg naar een JSON-bestand, die kan worden ingelezen door de acceptatieomgeving
+with open('dagtakenlijst_personeelslid_x.json', 'w') as json_bestand_uitvoer:
+    json.dump(dagtakenlijst, json_bestand_uitvoer, indent=4)
